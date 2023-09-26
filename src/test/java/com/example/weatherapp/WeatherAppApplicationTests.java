72cb3f84cd1a5a9bbf34c5395e90573716248bc8
luckill <81901449+luckill@users.noreply.github.com>
2023-09-25 20:43:38 -0700

    initialization

---
+++ src/test/java/com/example/weatherapp/WeatherAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.weatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
