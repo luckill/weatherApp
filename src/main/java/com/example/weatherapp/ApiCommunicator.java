e317d9d65a61838613db0aedf1ac59af2ca51a10
luckill <81901449+luckill@users.noreply.github.com>
2023-12-18 23:28:58 -0800

    updated ApiCommunicator.java and WeatherController.java

--- src/main/java/com/example/weatherapp/ApiCommunicator.java
+++ src/main/java/com/example/weatherapp/ApiCommunicator.java
@@ -1,23 +1,43 @@
 package com.example.weatherapp;
 
+import com.example.weatherapp.model.*;
 import org.springframework.http.*;
 import org.springframework.web.client.*;
 
+import java.util.*;
+
 public class ApiCommunicator
 {
-    public String getWeatherData(String latitude, String longitude, String key, String unit)
+    public String getData(String latitude, String longitude, String key, String unit, WeatherOption option)
     {
-        String url = "https://api.openweathermap.org/data/3.0/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely&units=" + unit + "&appid=" + key;
-        return getData(url, key);
+        String url;
+        if(option == WeatherOption.HOURLY)
+        {
+            url = "https://api.openweathermap.org/data/3.0/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,daily,current&units=" + unit + "&appid=" + key;
+
+        }
+        else
+        {
+            url = "https://api.openweathermap.org/data/3.0/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely&units=" + unit + "&appid=" + key;
+        }
+        return getDataFromAPI(url, key);
     }
 
-    public String getLocationData(String city, String key)
+    public String getData(String city, String key)
     {
         String url = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + key;
-        return getData(url, key);
+        return getDataFromAPI(url, key);
     }
 
-    private String getData(String url, String key)
+    public Map<String, String> getData(String latitude, String longitude, String key)
+    {
+        Map<String, String> airPollutionMap = new HashMap<>();
+        String url = "http://api.openweathermap.org/data/2.5/air_pollution?lat=" + latitude + "&lon=" + longitude + "&appid=" + key;
+        airPollutionMap.put("current", getDataFromAPI(url, key));
+        return airPollutionMap;
+    }
+
+    private String getDataFromAPI(String url, String key)
     {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", key);
--- src/main/java/com/example/weatherapp/Controller/WeatherController.java
+++ src/main/java/com/example/weatherapp/Controller/WeatherController.java
@@ -5,7 +5,7 @@ import com.example.weatherapp.model.*;
 import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.core.type.*;
 import com.fasterxml.jackson.databind.*;
-import org.apache.catalina.startup.*;
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
@@ -18,9 +18,9 @@ public class WeatherController
     public static String timeZone;
     private ApiCommunicator api = new ApiCommunicator();
     private ObjectMapper mapper = new ObjectMapper();
-    private Location location;
     private Map<String, String> airPollutionMap;
     private List<HourlyWeather> hourlyWeathers;
+    private Location location;
 
     private final ApiConfiguration apiConfiguration;
 
@@ -33,30 +33,38 @@ public class WeatherController
     @GetMapping("/")
     public String getWeatherData(Model model)
     {
-        String json = api.getData("Sacramento", apiConfiguration.AuthorizeToke());
+        String city = "San Francisco";
+        String json = api.getData(city, apiConfiguration.AuthorizeToke());
         try
         {
             mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-            List<Location> locations = mapper.readValue(json, new TypeReference<List<Location>>(){});
-            location = locations.get(0);
+            List<Location> locations = mapper.readValue(json, new TypeReference<List<Location>>()
+            {
+            });
+            if (locations.isEmpty())
+            {
+                model.addAttribute("errorMessage", "Invalid city!!! - no weather data available for the city entered");
+                return "index";
+            }
 
+            location = locations.get(0);
             airPollutionMap = api.getData(location.getLatitude(), location.getLongitude(), apiConfiguration.AuthorizeToke());
             AirQuality airQuality = mapper.treeToValue(mapper.readTree(airPollutionMap.get("current")).get("list").get(0).get("components"), AirQuality.class);
 
-            json = api.getData(location.getLatitude(), location.getLongitude(), apiConfiguration.AuthorizeToke(), "metric",WeatherOption.CURRENT);
+            json = api.getData(location.getLatitude(), location.getLongitude(), apiConfiguration.AuthorizeToke(), "metric", WeatherOption.CURRENT);
             JsonNode tree = mapper.readTree(json);
             timeZone = tree.path("timezone").asText();
 
             CurrentWeather currentWeather = mapper.treeToValue(tree.path("current"), CurrentWeather.class);
             currentWeather.initialize();
 
-            List<DailyWeather> dailyWeathers = mapper.readValue(tree.path("daily").toString(), new TypeReference<List<DailyWeather>>()
-            {
-            });
+            List<DailyWeather> dailyWeathers = mapper.readValue(tree.path("daily").toString(), new TypeReference<List<DailyWeather>>() {});
             for (DailyWeather weather : dailyWeathers)
             {
                 weather.initialize(weather.getTemp(), weather.getFeelLike());
             }
+
+            String hourly = tree.path("hourly").toString();
 
             model.addAttribute("airQuality", airQuality);
             model.addAttribute("location", location);
